912497e8a790d42f06e11816013344e5b5b00f0a
Amilcov <Amilcov@yahoo.com>
2022-04-22 13:37:52 +0300

    Add files via upload

---
+++ Week08/Day2/HTMLForm_Submit_Practice/starter/cat.js
@@ -0,0 +1,10 @@
+class Cat {
+  constructor({ pattern, name, size, description }) {
+    this.name = name;
+    this.pattern = pattern;
+    this.size = size;
+    this.description = description;
+  }
+}
+
+module.exports = Cat;
