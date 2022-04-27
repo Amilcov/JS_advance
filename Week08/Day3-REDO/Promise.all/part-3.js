0296b53d642b042b882cb33dfb7865329e5f0a50
Amilcov <Amilcov@yahoo.com>
2022-04-27 14:53:41 +0300

    Add files via upload

---
+++ Week08/Day3-REDO/Promise.all/part-3.js
@@ -0,0 +1,87 @@
+/*
+Write a function called `stretch`. It should:
+
+- return a promise
+- print `"done stretching"`
+- fulfill the promise after 1 second
+*/
+
+function stretch() {
+  return new Promise (resolve => {
+        setTimeout(() => {
+                           console.log('done stretching');
+                           resolve()
+                        }, 1000);
+        })
+
+}
+//stretch();
+
+
+/*
+Write a function called `runOnTreadmill`. It should:
+
+- return a promise
+- print `"done running on treadmill"`
+- fulfill the promise after 0.5 seconds
+*/
+
+function runOnTreadmill() {
+  return new Promise(resolve => {
+           setTimeout(() => {
+                              console.log('done running on trademill'); 
+                              resolve()
+                            }, 500);
+          
+        })
+}
+
+//runOnTreadmill();
+
+/*
+Write a function called `liftWeights`. It should:
+
+- return a promise
+- print `"done lifting weights"`
+- fulfill the promise after 2 seconds
+*/
+
+function liftWeights() {
+
+    return new Promise (resolve => {
+            setTimeout( () => {
+                                console.log('done lifting weights');
+                                resolve()
+                            }, 2000)
+
+        })
+}
+
+//liftWeights();
+
+/*
+Write a function called `workout` that runs the above functions in a way
+that ensures you begin running on the treadmill after you've finished
+stretching. Begin lifting weights after running on the treadmill. Print
+`"done working out"` after you've finished lifting weights.
+*/
+
+function workout() {
+    let activities = Promise.all([stretch(), runOnTreadmill(), liftWeights()]);
+    activities
+    .then(()=>'done working out')
+    .catch(reason => console.log('Error is: ', reason));
+}
+
+/*
+Test your code:
+
+Run the file (`node part-1.js`) and check your output against the expected output.
+*/
+
+
+workout();
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
